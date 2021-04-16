f1af45e2bec3c09348a75ad648165dc66fc11399
typchiienko <vt.progr@gmail.com>
2021-04-16 19:31:52 +0300

    Added fields and raw getters/setters to Product and User classes

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,53 @@
 package model;
 
 public class User {
+    private final int id;
+    private String userName;
+    private String password;
+    private boolean isBlocked;
+    private final UserRole userRole;
+
+    public User(String userName, String password, UserRole userRole) {
+        this.id = 0;
+        this.userName = userName;
+        this.password = password;
+        this.userRole = userRole;
+        this.isBlocked = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void block() {
+
+    }
+
+    public void unblock() {
+
+    }
 }
