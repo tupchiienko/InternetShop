31e96c30016d9e4510564bf7c0d47a9a64da1a42
VolodymyrTistechok <81178483+VolodymyrTistechok@users.noreply.github.com>
2021-04-21 22:20:21 +0300

    methods to the UserDaoImpl class

--- src/main/java/dao/impl/UserDaoImpl.java
+++ src/main/java/dao/impl/UserDaoImpl.java
@@ -2,53 +2,46 @@ package dao.impl;
 
 import dao.UserDao;
 import model.User;
+import model.UserRole;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 public class UserDaoImpl implements UserDao {
-    Map<Integer, User> userMap = new HashMap<>();
+    private Map<String, User> userMap = new HashMap<>();
 
     @Override
-    public void addUser(User user) {
-        userMap.put(user.getId(), user);
+    public Optional<User> add(User user) {
+        return Optional.ofNullable(userMap.put(user.getUsername(), user));
     }
 
     @Override
-    public List<User> getUser(String username) {
-        return userMap.values()
-                .stream()
-                .filter(i -> i.getUserName().equals(username))
-                .collect(toList());
+    public Optional<User> getByUsername(String username) {
+        return Optional.ofNullable(userMap.get(username));
     }
 
     @Override
-    public Optional<User> getUser(int id) {
-        if (userMap.containsKey(id)) {
-            return Optional.ofNullable(userMap.get(id));
-        }
-        return Optional.empty();
+    public Optional<User> update(String username, User newUser) {
+          return Optional.ofNullable(  userMap.replace(username, newUser));
     }
 
     @Override
-    public boolean updateUser(int id, User newUser) {
-        if (userMap.containsKey(id)) {
-            userMap.replace(id, newUser);
-            return true;
-        }
-        return false;
+    public Optional<User> delete(String username) {
+        return  Optional.ofNullable(userMap.remove(username));
     }
 
     @Override
-    public boolean deleteUser(int id) {
-        if (userMap.containsKey(id)) {
-            userMap.remove(id);
-            return true;
-        }
-        return false;
+    public Map<String, User> getByRole(UserRole userRole) {
+       return userMap.values().stream()
+                .filter(user -> user.getUserRole().equals(userRole))
+                .collect(toMap(User::getUsername, User -> User));
+    }
+
+    @Override
+    public Map<String, User> getAllUsers() {
+        return userMap;
     }
 }
