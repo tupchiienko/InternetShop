1f16ed92b7c694a7ad634bc181ebbce9318d808f
Oleksandr Sheremeta <sheremeta.sasha.2000@gmail.com>
2021-05-03 18:03:42 +0300

    OrderDaoImpl

--- src/main/java/dao/impl/OrderDaoImpl.java
+++ src/main/java/dao/impl/OrderDaoImpl.java
@@ -7,30 +7,47 @@ import model.User;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
+
+import static java.util.stream.Collectors.toMap;
 
 public class OrderDaoImpl implements OrderDao {
+    private final Map<Integer, Order> orderMap;
+
+    public OrderDaoImpl() {
+        orderMap = new TreeMap<>();
+    }
+
     @Override
     public Optional<Order> add(Order order) {
-        return Optional.empty();
+        return Optional.ofNullable(orderMap.put(order.getId(), order));
     }
 
     @Override
     public Optional<Order> getById(int id) {
-        return Optional.empty();
+        return Optional.ofNullable(orderMap.get(id));
     }
 
     @Override
     public Optional<Order> update(int id, Order newOrder) throws OrderDataException {
-        return Optional.empty();
+        Optional<Order> deletedOrder = delete(id);
+        if (deletedOrder.isPresent()) {
+            add(newOrder);
+        }
+        return deletedOrder;
     }
 
     @Override
     public Optional<Order> delete(int id) {
-        return Optional.empty();
+        return Optional.ofNullable(orderMap.remove(id));
     }
 
     @Override
     public Map<Integer, Order> getByUser(User user) {
-        return null;
+        return orderMap.values().stream()
+                .filter(order -> order.getUser().equals(user))
+                .collect(toMap(Order::getId, Order -> Order));
+
+
     }
 }
