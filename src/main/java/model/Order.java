a6d912a437b5cf7c4893422eb264b369adbeba96
typchiienko <vt.progr@gmail.com>
2021-05-03 15:53:01 +0300

    Order class methods

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -3,8 +3,11 @@ package model;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Order {
+    private static int idCounter;
+
     private int id;
     private final User user;
     private final Map<Product, Integer> productMap;
@@ -12,9 +15,10 @@ public class Order {
     private OrderStatus orderStatus;
 
     public Order(User user) {
+        this.id = ++idCounter;
         this.user = user;
         this.productMap = new HashMap<>();
-        calculateTotalPrice();
+        this.totalPrice = new BigDecimal(0);
         this.orderStatus = OrderStatus.IN_PROGRESS;
     }
 
@@ -34,24 +38,32 @@ public class Order {
         return new HashMap<>(productMap);
     }
 
-    public void addProduct(Product product) {
+    public boolean containsProduct(Product product) {
+        return productMap.containsKey(product);
+    }
 
+    public void addProduct(Product product, int count) {
+        productMap.put(product, count);
+        totalPrice = totalPrice.add(product.getPrice().multiply(new BigDecimal(count)));
     }
 
     public void removeProduct(Product product) {
-
+        productMap.remove(product);
+        totalPrice = totalPrice.subtract(product.getPrice().multiply(new BigDecimal(productMap.get(product))));
     }
 
-    public void changeProductCount(Product product, int count) {
-
+    public void changeProductCount(Product product, int newCount) {
+        @SuppressWarnings("ConstantConditions")
+        int currentCount = productMap.replace(product, newCount);
+        if (currentCount < newCount) {
+            totalPrice = totalPrice.add(product.getPrice().multiply(new BigDecimal(newCount - currentCount)));
+        } else if (currentCount > newCount) {
+            totalPrice = totalPrice.subtract(product.getPrice().multiply(new BigDecimal(currentCount - newCount)));
+        }
     }
 
     public BigDecimal getTotalPrice() {
         return totalPrice;
-    }
-
-    private void calculateTotalPrice() {
-        this.totalPrice = new BigDecimal(0);
     }
 
     public OrderStatus getOrderStatus() {
@@ -60,5 +72,28 @@ public class Order {
 
     public void setOrderStatus(OrderStatus orderStatus) {
         this.orderStatus = orderStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id == order.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", user=" + user +
+                ", totalPrice=" + totalPrice +
+                ", orderStatus=" + orderStatus +
+                '}';
     }
 }
