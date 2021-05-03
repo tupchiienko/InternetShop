c10519acf92f4c203261485b11b522c2ca1be487
typchiienko <vt.progr@gmail.com>
2021-05-03 16:49:42 +0300

    Changed OrderService methods signature, OrderServiceImpl class methods realized

--- src/main/java/service/impl/OrderServiceImpl.java
+++ src/main/java/service/impl/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package service.impl;
 
+import dao.OrderDao;
+import dao.impl.OrderDaoImpl;
 import model.Order;
 import model.OrderStatus;
 import model.Product;
@@ -8,40 +10,96 @@ import service.OrderService;
 import service.Response;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class OrderServiceImpl implements OrderService {
+    private final OrderDao orderDao;
+
+    public OrderServiceImpl() {
+        this.orderDao = new OrderDaoImpl();
+    }
+
     @Override
     public Response<Order> getOrder(int id) {
-        return null;
+        Optional<Order> byIdOptional = orderDao.getById(id);
+        return byIdOptional
+                .map(value -> new Response<>(value, true, value.toString()))
+                .orElseGet(() -> new Response<>(null, false,
+                        "Order with id `" + id + " does not exist"));
     }
 
     @Override
     public Response<Order> deleteOrder(int id) {
-        return null;
+        Optional<Order> deletedOrderOptional = orderDao.delete(id);
+        return deletedOrderOptional
+                .map(value -> new Response<>(value, true,
+                        "Order with id `" + id + " deleted successfully"))
+                .orElseGet(() -> new Response<>(null, false,
+                        "Order with id `" + id + " does not exist"));
     }
 
     @Override
     public Response<Map<Integer, Order>> getOrdersByUser(User user) {
-        return null;
+        Map<Integer, Order> byUserMap = orderDao.getByUser(user);
+        if (byUserMap.isEmpty()) {
+            return new Response<>(null, false,
+                    "User '" + user.getUsername() + "' has no orders");
+        }
+        return new Response<>(byUserMap, true,
+                "User has orders with id '" + byUserMap.keySet() + "'");
     }
 
     @Override
     public Response<Order> addOrder(Order order) {
-        return null;
+        Optional<Order> orderOptional = orderDao.getById(order.getId());
+        if (orderOptional.isEmpty()) {
+            orderDao.add(order);
+            return new Response<>(order, true,
+                    "Order '" + order.getId() + "' successfully created");
+        }
+        return new Response<>(null, false,
+                "Order '" + order.getId() + "' already exist");
     }
 
     @Override
     public Response<Order> changeOrderStatus(int id, OrderStatus orderStatus) {
-        return null;
+        Optional<Order> orderOptional = orderDao.getById(id);
+        if (orderOptional.isPresent()) {
+            Order orderToChange = orderOptional.get();
+            orderToChange.setOrderStatus(orderStatus);
+            orderDao.update(id, orderToChange);
+            return new Response<>(orderToChange, true,
+                    "Order '" + id + "' successfully updated");
+        }
+        return new Response<>(null, false,
+                "Order with id `" + id + " does not exist");
     }
 
     @Override
-    public Response<Order> addProductToOrder(int id, Product product) {
-        return null;
+    public Response<Order> addProductToOrder(int id, Product product, int count) {
+        Optional<Order> orderOptional = orderDao.getById(id);
+        if (orderOptional.isPresent()) {
+            Order orderToChange = orderOptional.get();
+            orderToChange.addProduct(product, count);
+            orderDao.update(id, orderToChange);
+            return new Response<>(orderToChange, true,
+                    "Order '" + id + "' successfully updated");
+        }
+        return new Response<>(null, false,
+                "Order with id `" + id + " does not exist");
     }
 
     @Override
     public Response<Order> removeProductFormOrder(int id, Product product) {
-        return null;
+        Optional<Order> orderOptional = orderDao.getById(id);
+        if (orderOptional.isPresent()) {
+            Order orderToChange = orderOptional.get();
+            orderToChange.removeProduct(product);
+            orderDao.update(id, orderToChange);
+            return new Response<>(orderToChange, true,
+                    "Order '" + id + "' successfully updated");
+        }
+        return new Response<>(null, false,
+                "Order with id `" + id + " does not exist");
     }
 }
